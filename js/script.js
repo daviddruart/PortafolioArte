3eee9932ec5826a2a4f00c0f956df2cea50c3748
daviddruart <davidrincondru08@gmail.com>
2026-03-16 22:42:40 -0500

    mas cositas

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
-document.getElementById("btnInicio").addEventListener("click", function(){
+const boton = document.getElementById("btnInicio");
+const menu = document.getElementById("menu");
+const contenedorBoton = document.getElementById("contenedorBoton");
 
-    window.scrollTo({
-        top: window.innerHeight,
-        behavior: "smooth"
-    });
+boton.addEventListener("click", function(e){
+
+menu.classList.add("menu-activo");
+
+contenedorBoton.classList.add("mover");
+
+e.stopPropagation();
+
+});
+
+
+document.addEventListener("click", function(event){
+
+const clickDentroMenu = menu.contains(event.target);
+const clickBoton = boton.contains(event.target);
+
+if(!clickDentroMenu && !clickBoton){
+
+menu.classList.remove("menu-activo");
+
+contenedorBoton.classList.remove("mover");
+
+}
 
 });
