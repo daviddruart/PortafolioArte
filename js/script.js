d710793920725a2443ddb05e8ed962c9ec21c34d
daviddruart <davidrincondru08@gmail.com>
2026-03-17 23:51:27 -0500

    mas ajustes

--- js/script.js
+++ js/script.js
@@ -1,29 +1,16 @@
 const boton = document.getElementById("btnInicio");
 const menu = document.getElementById("menu");
-const contenedorBoton = document.getElementById("contenedorBoton");
 
 boton.addEventListener("click", function(e){
-
-menu.classList.add("menu-activo");
-
-contenedorBoton.classList.add("mover");
-
-e.stopPropagation();
-
+    menu.classList.add("menu-activo");
+    e.stopPropagation();
 });
 
-
 document.addEventListener("click", function(event){
+    const clickDentroMenu = menu.contains(event.target);
+    const clickBoton = boton.contains(event.target);
 
-const clickDentroMenu = menu.contains(event.target);
-const clickBoton = boton.contains(event.target);
-
-if(!clickDentroMenu && !clickBoton){
-
-menu.classList.remove("menu-activo");
-
-contenedorBoton.classList.remove("mover");
-
-}
-
+    if(!clickDentroMenu && !clickBoton){
+        menu.classList.remove("menu-activo");
+    }
 });
